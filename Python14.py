d4555243be4f71362453b8928cc9a26a478c8591
Aakash-lpu-9961 <aakashkumarjh9961@gmail.com>
2021-09-10 00:48:40 +0530

    fourth commit

--- Python14.py
+++ Python14.py
@@ -1,7 +1,7 @@
 print("Hello , How are you Sir")
 p=25
 q=36
-if id(p)==id(q): print("sucess")
+if id(p)==id(q): print("success")
 r=10
 s=10
-if id(r)==id(s):print("Sucess")
+if id(r)==id(s):print("Success")
---
+++ Python15.py
@@ -0,0 +1,9 @@
+# Python Program to understand the concept of Numbers
+print("Enter the any number you want: ")
+p=int(input("Enter the number: "))
+print("You entered the number", p ,"Okay!!!")
+print("Number type conversion")
+print("Int to Float :", float(p))
+print("Int to \a Complex: ", complex(p))
+print("Mathematical functions") 
+print(u'Hello, World')
