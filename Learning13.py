444ac5e5aa26f0aaa9d20edf95ddb05e448296c7
Aakash-lpu-9961 <aakashkumarjh9961@gmail.com>
2022-01-05 00:31:08 +0530

    Ninth Commit

--- Learning13.py
+++ Learning13.py
@@ -1,2 +1,35 @@
 # Functions and Docstrings in Python
-print(" Test Program Learning13.py")
+print("Test Program Learning13.py \n\n\n")
+
+print("Functions in Python\n\n")
+
+print("built-in functions")
+a = 9
+b = 7
+c = sum((a, b))  # built-in function
+print("Value of c is: ", c, "\n\n\n\n")
+
+print("User defined functions")
+
+
+def function(d, e):
+    """ This is a function which will calculate average of two numbers"""  # Ye docstring hai comment ni hai
+    average = (d + e) / 2
+    print(average)  # This function will print the value of average
+    return average
+
+
+""" 
+    Lekin agar ham iske value ko kisi variable me store karana chahte hai tb hame return() statement 
+    ka use krna hoga
+"""
+
+
+
+v = function(5, 7)
+print("Value of v using return statement:\t", v)
+
+print(function.__doc__)
+
+"""By help of .__doc__ we can easily know that what the 
+objective of the function and why we have created that function"""
